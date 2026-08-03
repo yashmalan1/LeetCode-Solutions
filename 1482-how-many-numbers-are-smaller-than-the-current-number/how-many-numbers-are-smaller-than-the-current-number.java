b89b189d0529bd02820bcbb8d813b756bdfe4c36
yashmalan1 <yashmalan.work@gmail.com>
2026-08-03 20:29:44 +0530

    Time: 8 ms (64.02%) | Memory: 45.7 MB (35.73%) - LeetSync

---
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (nums[j] < nums[i]) {
+                    count = count + 1;
+                }
+            }
+            result[i] = count;
+        }
+        
+        return result;
+    }
+}
