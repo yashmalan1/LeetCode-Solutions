ac4dc62db340f37085dadd46045440f07eb6a8a2
yashmalan1 <yashmalan.work@gmail.com>
2026-08-29 22:53:21 +0530

    Time: 19 ms (28.09%) | Memory: 46.1 MB (18.82%) - LeetSync

---
+++ 3507-find-the-count-of-numbers-which-are-not-special/find-the-count-of-numbers-which-are-not-special.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int nonSpecialCount(int l, int r) {
+        int r_root=(int)Math.sqrt(r);
+
+        boolean isPrime[]=new boolean[r_root+1];
+        Arrays.fill(isPrime,true);
+
+        isPrime[0]=false;
+        isPrime[1]=false;
+        for(int i=2;i<=r_root;i++){
+            if(isPrime[i]){
+                for(int j=i*i;j<=r_root;j+=i){
+                    isPrime[j]=false;
+                }
+            }
+        }
+        int count=0;
+        for(int i=2;i<=r_root;i++){
+            if(isPrime[i] && i*i<=r && i*i>=l){
+                count++;
+            }
+        }
+        return (r-l+1)-count;
+    }
+}
