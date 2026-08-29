0473939f25e56409db185b96289074bb70997b1a
yashmalan1 <yashmalan.work@gmail.com>
2026-08-29 22:59:31 +0530

    Time: 15 ms (37.08%) | Memory: 46 MB (46.35%) - LeetSync

--- 3507-find-the-count-of-numbers-which-are-not-special/find-the-count-of-numbers-which-are-not-special.java
+++ 3507-find-the-count-of-numbers-which-are-not-special/find-the-count-of-numbers-which-are-not-special.java
@@ -1,6 +1,7 @@
 class Solution {
     public int nonSpecialCount(int l, int r) {
         int r_root=(int)Math.sqrt(r);
+        int l_root=(int)Math.sqrt(l);
 
         boolean isPrime[]=new boolean[r_root+1];
         Arrays.fill(isPrime,true);
@@ -15,7 +16,7 @@ class Solution {
             }
         }
         int count=0;
-        for(int i=2;i<=r_root;i++){
+        for(int i=l_root;i<=r_root;i++){
             if(isPrime[i] && i*i<=r && i*i>=l){
                 count++;
             }
