8fd7917b27373601f3681fb0985a798a4325aa5a
yashmalan1 <yashmalan.work@gmail.com>
2026-08-31 15:55:23 +0530

    Time: 36 ms (29.30%) | Memory: 59.3 MB (31.97%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -16,8 +16,8 @@ class Solution {
                     j++;
                     k--;
 
-                    while (j < k && nums[j] == nums[j - 1]) j++;
-                    while (j < k && nums[k] == nums[k + 1]) k--;
+                    while (j<k && nums[j]==nums[j-1]) j++;
+                    while (j<k && nums[k]==nums[k+1]) k--;
                 }
                 else if(sum<target){
                     j++;
