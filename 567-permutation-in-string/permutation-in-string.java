ab41c5b04911fab346610e035fcc35eef8456ff8
yashmalan1 <yashmalan.work@gmail.com>
2026-09-13 17:18:03 +0530

    Time: 36 ms (17.46%) | Memory: 46.8 MB (17.48%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,28 +1,31 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if(s1.length() > s2.length()) return false;
-        HashMap<Character,Integer> map1 = new HashMap<>();
-        HashMap<Character,Integer> map2 = new HashMap<>();
-        for(int i=0;i<s1.length();i++){
-            char ch=s1.charAt(i);
-            char ch1=s2.charAt(i);
-            map1.put(ch,map1.getOrDefault(ch,0)+1);
-            map2.put(ch1,map2.getOrDefault(ch1,0)+1);            
+        if (s1.length() > s2.length())
+            return false;
+        HashMap<Character, Integer> map1 = new HashMap<>();
+        HashMap<Character, Integer> map2 = new HashMap<>();
+        for (int i = 0; i < s1.length(); i++) {
+            char ch = s1.charAt(i);
+            char ch1 = s2.charAt(i);
+            map1.put(ch, map1.getOrDefault(ch, 0) + 1);
+            map2.put(ch1, map2.getOrDefault(ch1, 0) + 1);
         }
-            if(map1.equals(map2)) return true;
+        if (map1.equals(map2))
+            return true;
 
-        int ei=s1.length(),si=0;
-        while(ei<s2.length()){
+        int ei = s1.length(), si = 0;
+        while (ei < s2.length()) {
             char old = s2.charAt(si);
 
-            if(map2.get(old) == 1){
+            if (map2.get(old) == 1) {
                 map2.remove(old);
-            }else{
-                map2.put(old,map2.get(old)-1);
+            } else {
+                map2.put(old, map2.get(old) - 1);
             }
             char add = s2.charAt(ei);
-            map2.put(add, map2.getOrDefault(add,0)+1);
-            if(map1.equals(map2)) return true;
+            map2.put(add, map2.getOrDefault(add, 0) + 1);
+            if (map1.equals(map2))
+                return true;
             ei++;
             si++;
         }
